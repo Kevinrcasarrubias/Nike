9d269ae06d6f804fa5b82da0050f87786e7bdf48
99162919+Kevinrcasarrubias@users.noreply.github.com <99162919+Kevinrcasarrubias@users.noreply.github.com>
2023-10-23 17:05:43 -0600

    1.0

---
+++ src/sections/index.js
@@ -0,0 +1,20 @@
+import SuperQuality from "./SuperQuality";
+import PopularProducts from "./PopularProducts";
+import Hero from "./Hero";
+import CustomerReviews from "./CustomerReviews";
+import Footer from "./Footer";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import Suscribe from "./Suscribe";
+
+
+export {
+    Hero,
+    SuperQuality,
+    PopularProducts,
+    CustomerReviews,
+    Footer,
+    Services,
+    SpecialOffer,
+    Suscribe
+}
