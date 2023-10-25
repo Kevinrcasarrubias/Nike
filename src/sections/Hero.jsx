0a475b883b011eb4e068e34a99ef559227de18af
99162919+Kevinrcasarrubias@users.noreply.github.com <99162919+Kevinrcasarrubias@users.noreply.github.com>
2023-10-25 09:44:15 -0600

    7.3

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -20,7 +20,7 @@ const Hero = () => {
          <p className="text-xl font-montserrat text-coral-red">
            Our Summer Collections
           </p>
-         <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82] font-bold">
+         <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
             <span
               className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10"
             >
@@ -42,9 +42,9 @@ const Hero = () => {
             label="Shop now"
             iconURL={arrowRight} 
         />
-        <div className="flex justify-starts items-start flex-wrap w-full mt-20 gap-16">
-            {statistics.map ((stat) => (
-              <div key={stat.label}>
+        <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16">
+            {statistics.map ((stat, index) => (
+              <div key={index}>
                 <p className="text-4xl font-palanquin font-bold">{stat.value}</p>
                 <p className="leading-7 font-montserrat text-slate-gray">{stat.label}</p>
               </div>
@@ -52,12 +52,12 @@ const Hero = () => {
         </div>
       </div>
 
-      <div className="relative flex-1 flex juistify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
+      <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
           <img 
             src={bigShoeImg}
             alt="shoe collection"
             width={610}
-            height={500}
+            height={502}
             className="object-contain relative z-10"
         />
 
